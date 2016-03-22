f8508e8af0d302adecaef69d1016889fc6edb972
Maurice <makers@posteo.ch>
2016-03-22 19:08:58 +0100

    pin out seperated

--- FirmWare/FirmWare.ino
+++ FirmWare/FirmWare.ino
@@ -5,13 +5,7 @@
 #include <Wire.h>         //http://arduino.cc/en/Reference/Wire (included with Arduino IDE)
 #include <TM1637.h>
 
-//Pin Out
-const int button1Pin = 10; //Button to switch between times
-const int button2Pin = 11; //Up Button
-const int button3Pin = 12; //Down Button
-const int ledPin = 13; //Indicator LED
-const int powerPin = 2; //Ballast Power Interrupt Signal
-const int switchPin = 3; //Bulb Switching Signal
+#include "pinOut.h"
 
 //RTC Check Counter
 float rtcCheck = 0;
---
+++ FirmWare/pinOut.h
@@ -0,0 +1,7 @@
+//Pin Out
+const int button1Pin = 10; //Button to switch between times
+const int button2Pin = 11; //Up Button
+const int button3Pin = 12; //Down Button
+const int ledPin = 13; //Indicator LED
+const int powerPin = 2; //Ballast Power Interrupt Signal
+const int switchPin = 3; //Bulb Switching Signal
